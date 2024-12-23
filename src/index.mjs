85985480bccc22bbd3d974a76f323ec0e5074a3e
technomancer <bozios11@gmail.com>
2024-12-23 16:48:23 +0100

    session 2

--- src/index.mjs
+++ src/index.mjs
@@ -10,7 +10,7 @@ const app = express();
 app.use(cookieParser()); 
 app.use(session({secret:'ansonaisthedibal123',resave:false,cookie: {
     maxAge: 60000 * 60,
-},visited:true}));
+},visited:true,saveUninitialized:true}));
 app.use(express.json());  // Fix to correctly parse incoming JSON bodies
 app.use(userRouters); // user routes 
 
@@ -34,6 +34,21 @@ app.get("/", loggingMiddleware, (request, response) => {
     
 });
 
+app.post('/api/auth', (request,response) => {
+    const {body: { username, password},} = request;
+    
+    const findUser = mockusers.find( (user) => user.name === username);
+    
+    if(!findUser || findUser.password !== password) return response.status(401).send({msg: "Bad creaditials"});
+    
+    request.session.user = findUser;
+    return response.status(200).send(findUser);
+})
+
+app.get('/api/auth', (request,response) => {
+    
+    return request.session.user ? response.send(200).send(request.session.user) : response.send(401).send({msg: "Bad credentials"});
+})
 
 
 
